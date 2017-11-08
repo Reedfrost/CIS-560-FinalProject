871a25cdae3f7e23e3eb0272fd392489e02aa1e5
badonmap <badonmap@ksu.edu>
2017-11-08 17:06:19 -0600

    base python file is created

---
+++ frontend/main.py
@@ -0,0 +1,10 @@
+class assign:
+    def display():
+	    print("display")
+    def menu(id, query):
+	    pass
+def main():
+    print("hello")
+    assign.display()
+if __name__ == '__main__':
+        main()
